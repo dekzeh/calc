19151a8a9fed7777e3c9fc3129dc6307d0d866c7
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2023-08-11 19:20:17 +0200

    added dynamic truck message (with dynamic strings yoo) and small fix onload

---
+++ dist/js/data/truck.js
@@ -0,0 +1,5 @@
+TRUCK_MESSAGES = [
+    "Doing this will select the first trainer and wipe out your mons, do you really wish to do that?",
+    "Doing this will select the first trainer and wipe out your mons, do you really wish to do that?",
+    ()=>{return `You wiped to ${window.CURRENT_TRAINER} ? Hey there, I know you just lost your run so take it easy and think in the Truck about your next moves against these opponents`},
+]
